fe035a26087a4547f9452b19a36d5dad52a09d6c
Raduna Daniel <danielraduna12@gmail.com>
2023-05-26 08:33:27 +0300

    second

--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/PracticalTest02MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/PracticalTest02MainActivity.java
@@ -3,12 +3,52 @@ package ro.pub.cs.systems.eim.practicaltest02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class PracticalTest02MainActivity extends AppCompatActivity {
+
+    private EditText serverPortEditText, clientAddressEditText, clientPortEditText;
+    private EditText number1EditText, number2EditText;
+    private Button connectButton, addButton, multiplyButton;
+    private TextView resultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test02_main);
+
+        serverPortEditText = findViewById(R.id.server_port_edit_text);
+        clientAddressEditText = findViewById(R.id.client_address_edit_text);
+        clientPortEditText = findViewById(R.id.client_port_edit_text);
+        number1EditText = findViewById(R.id.number1_edit_text);
+        number2EditText = findViewById(R.id.number2_edit_text);
+        connectButton = findViewById(R.id.connect_button);
+        addButton = findViewById(R.id.button_add);
+        multiplyButton = findViewById(R.id.button_multiply);
+        resultTextView = findViewById(R.id.weather_forecast_text_view);
+
+        connectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Implement the functionality to connect the client to the server
+            }
+        });
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Implement the functionality to perform the addition operation
+            }
+        });
+
+        multiplyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Implement the functionality to perform the multiplication operation
+            }
+        });
     }
 }
